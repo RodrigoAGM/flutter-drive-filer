16a908dee3ad5900d3d36b8a47756902ae9e6937
RodrigoAGM <rod-gm@hotmail.com>
2019-04-15 00:12:46 -0500

    Onboarding dot indicators added

---
+++ lib/Onboarding/dot_indicators.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DotsIndicator extends AnimatedWidget {
+  DotsIndicator({
+    this.controller,
+    this.itemCount,
+    this.onPageSelected,
+    this.color: Colors.white,
+  }) : super(listenable: controller);
+
+  final PageController controller;
+  final int itemCount;
+
+  final ValueChanged<int> onPageSelected;
+  final Color color;
+
+  static const double _kDotSize = 8.0;
+  static const double _kMaxZoom = 2.0;
+  static const double _kDotSpacing = 25.0;
+
+  Widget _buildDot(int index) {
+    double selecteddot = Curves.easeOut.transform(
+      max(
+        0.0,
+        1.0 - ((controller.page ?? controller.initialPage) - index).abs(),
+      ),
+    );
+    double zoom = 1.0 + (_kMaxZoom - 1.0) * selecteddot;
+    return new Container(
+      width: _kDotSpacing,
+      child: new Center(
+        child: new Material(
+          color: color,
+          type: MaterialType.circle,
+          child: new Container(
+            width: _kDotSize * zoom,
+            height: _kDotSize * zoom,
+            child: new InkWell(
+              onTap: () => onPageSelected(index),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget build(BuildContext context) {
+    return new Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: new List<Widget>.generate(itemCount, _buildDot),
+    );
+  }
+}
--- lib/Onboarding/page.dart
+++ lib/Onboarding/page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_drive_filer/UI/circles_with_image.dart';
+import 'package:flutter_drive_filer/Onboarding/circles_with_image.dart';
 
 class Page extends StatelessWidget {
 
