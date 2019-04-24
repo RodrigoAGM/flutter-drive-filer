2f105a2e2ea68187ae8e296e1304d7d9553a385a
RodrigoAGM <rod-gm@hotmail.com>
2019-04-24 11:47:08 -0500

    File model class created

---
+++ lib/domain/model/drive_file.dart
@@ -0,0 +1,14 @@
+
+class DriveFile {
+  String id;
+  String name;
+  double size;
+  List<String> parents;
+
+  DriveFile({
+    this.id,
+    this.name,
+    this.size,
+    this.parents,
+  });
+}
